2db5c3de5e10f1616eac0d1ce2dd3a4a4996bbd2
alisena <alisenamudaber@gmail.com>
2019-05-07 06:04:25 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/basic-dashboard/precache-manifest.e6b443b4cb02f7b4bc40b1e7347b41c0.js"
+  "/basic-dashboard/precache-manifest.caf9471eb2793f8329a55388ecbaed73.js"
 );
 
 workbox.clientsClaim();
